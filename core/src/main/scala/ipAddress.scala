22c04e66c1f88beaab3e33039f8035313df1cafd
Ian Murray <ian.c.murray@gmail.com>
2014-09-28 20:24:32 +0100

    (core) Oops, can't count

--- core/src/main/scala/ipAddress.scala
+++ core/src/main/scala/ipAddress.scala
@@ -4,4 +4,4 @@ package harvester
 
 sealed trait IPAddress
 case class IP4Address(raw: (Byte, Byte, Byte, Byte)) extends IPAddress
-case class IP6Address(raw: (Byte, Byte, Byte, Byte)) extends IPAddress
+case class IP6Address(raw: (Byte, Byte, Byte, Byte, Byte, Byte)) extends IPAddress
