92c1bc20d595b438e35505ee40bbd26877fa56aa
Ian Murray <ian.c.murray@gmail.com>
2014-09-28 20:18:28 +0100

    (core) Add timeout info to Targets

--- core/src/main/scala/target.scala
+++ core/src/main/scala/target.scala
@@ -4,7 +4,20 @@ package harvester
 
 import scala.concurrent.duration._
 
-sealed trait Target
-case object Once extends Target
-case class OnceAfter(delay: Duration) extends Target
-case class Every(interval: Duration) extends Target
+sealed trait Target {
+  def timeout: FiniteDuration
+}
+
+case class Once(timeout: FiniteDuration) extends Target
+case class Every(interval: FiniteDuration, grace: FiniteDuration) extends Target {
+  def timeout = interval + grace
+}
+case class Within(
+    window: (FiniteDuration, FiniteDuration),
+    grace: FiniteDuration) extends Target {
+
+  def timeout = upper + grace
+  def lower = window._1
+  def upper = window._2
+  def midpoint = (lower+upper) / 2
+}
