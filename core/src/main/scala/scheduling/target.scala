d112423cbabedb3f1d6432bf77d007792af1dd84
Ian Murray <ian.c.murray@gmail.com>
2014-10-22 13:47:46 +0100

    (core) Restrict access to Target a little.
    Kept in package-accessible for the composing Schedules, but public
    access should only need to go through the "time now" methods.

--- core/src/main/scala/scheduling/target.scala
+++ core/src/main/scala/scheduling/target.scala
@@ -5,17 +5,18 @@ package scheduling
 import scala.concurrent.duration._
 
 trait TargetLike {
-  val initiateAt: Deadline
-  val timeoutAt: Deadline
 
   def initialDelay(): FiniteDuration = initialDelayFrom(Instant.now())
   def timeoutDelay(): FiniteDuration = timeoutDelayFrom(Instant.now())
 
-  def initialDelayFrom(now: Instant) = {
+  protected[scheduling] val initiateAt: Deadline
+  protected[scheduling] val timeoutAt: Deadline
+
+  protected[scheduling] def initialDelayFrom(now: Instant) = {
     initiateAt.timeLeftAt(now) max Duration.Zero
   }
 
-  def timeoutDelayFrom(now: Instant) = {
+  protected[scheduling] def timeoutDelayFrom(now: Instant) = {
     timeoutAt.timeLeftAt(now) max Duration.Zero
   }
 }
