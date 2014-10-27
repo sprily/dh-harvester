df111217c2622e877bf13364b163832251497e11
Ian Murray <ian.c.murray@gmail.com>
2014-10-27 15:10:58 +0000

    (core) Get rid of IPv6 for now
    I clearly didn't know how to represent it anyway, as it should be
    8 groups of 16 bits!

--- core/src/main/scala/network/ipAddress.scala
+++ core/src/main/scala/network/ipAddress.scala
@@ -2,6 +2,13 @@ package uk.co.sprily.dh
 package harvester
 package network
 
-sealed trait IPAddress
-case class IP4Address(raw: (Byte, Byte, Byte, Byte)) extends IPAddress
-case class IP6Address(raw: (Byte, Byte, Byte, Byte, Byte, Byte)) extends IPAddress
+import java.net.InetAddress
+
+sealed trait IPAddress {
+  def bytes: Seq[Byte]
+  def inet: InetAddress = InetAddress.getByAddress(bytes.toArray)
+}
+
+case class IP4Address(raw: (Byte, Byte, Byte, Byte)) extends IPAddress {
+  def bytes = List(raw._1, raw._2, raw._3, raw._4) 
+}
