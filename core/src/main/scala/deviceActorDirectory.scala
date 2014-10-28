bba4e60372d76ea04241041d7f6c6917c22f7d1c
Ian Murray <ian.c.murray@gmail.com>
2014-10-28 15:13:41 +0000

    (core) WIP defining a general implentation of the actor directory.
    Still needs to handle failures in the children correctly.

--- core/src/main/scala/deviceActorDirectory.scala
+++ core/src/main/scala/deviceActorDirectory.scala
@@ -22,3 +22,45 @@ trait DeviceActorDirectory[D <: Device] {
 
   def lookup(device: D): ActorSelection
 }
+
+abstract class DeviceActorDirectoryImpl[D <: Device]
+                                       (system: ActorSystem)
+                                          extends DeviceActorDirectory[D] {
+
+  import Protocol.Poll
+  private val router = system.actorOf(Props(new Router()))
+  override def lookup(device: D) = system.actorSelection(router.path)
+
+  /** Abstract members to define how to group Devices by network location. */
+  protected type NetLoc
+  protected def netLocFor(device: D): NetLoc
+
+  /** How to create a new actor responsible for communicating with
+    * the given network location.
+    */
+  protected def workerProps(netLoc: NetLoc): Props
+
+  /** An Actor which forwards Poll messages to the actor capable of
+    * fulfilling the request.
+    *
+    * Creates new child actors as necessary.
+    */
+  private class Router extends Actor with ActorLogging {
+
+    private var locs = Map[NetLoc, ActorRef]()
+
+    def receive = {
+      case p@Poll(device, _) => lookupActor(device) forward p
+    }
+
+    def lookupActor(d: D): ActorRef = {
+      val netLoc = netLocFor(d)
+      if(! locs.contains(netLoc)) {
+        val a = context.actorOf(workerProps(netLoc))
+        locs += netLoc -> a
+      }
+
+      locs.get(netLoc).get
+    }
+  }
+}
