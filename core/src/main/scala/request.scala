25990c7efea0ae130d8a50380573c9f9af01fa41
Ian Murray <ian.c.murray@gmail.com>
2014-11-06 10:31:33 +0000

    (core) AdhocRequest definition

--- core/src/main/scala/request.scala
+++ core/src/main/scala/request.scala
@@ -3,9 +3,16 @@ package harvester
 
 import network.Device
 import scheduling.Schedule
+import scheduling.TargetLike
 
 case class PersistentRequest[D <: Device](
     val id: Long,
     val schedule: Schedule,
+    val device: D,
+    val selection: D#AddressSelection)
+
+case class AdhocRequest[D <: Device](
+    val id: Long,
+    val target: TargetLike,
     val device: D,
     val selection: D#AddressSelection)
