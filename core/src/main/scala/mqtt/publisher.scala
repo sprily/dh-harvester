755a72bcbba2b7eabac0eea6472c19662fd64dac
Ian Murray <ian.c.murray@gmail.com>
2014-11-09 21:10:03 +0000

    (core) Use scala-mqtt to publish to the broker.

--- core/src/main/scala/mqtt/publisher.scala
+++ core/src/main/scala/mqtt/publisher.scala
@@ -6,12 +6,15 @@ import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.actor.Props
 
+import uk.co.sprily.mqtt.AsyncSimpleClient
+
 import network.Device
 
 class ResultsPublisher[D <: Device](
     topicRoot: String,
     device: D,
-    bus: DeviceBus)
+    bus: DeviceBus,
+    mqttClient: AsyncSimpleClient.Client)
     (implicit serialiser: Serialiser[D#Measurement]) extends Actor with ActorLogging {
 
   // akka hooks.
@@ -32,8 +35,11 @@ class ResultsPublisher[D <: Device](
 }
 
 object ResultsPublisher {
-  def props[D <: Device](topicRoot: String, d: D, bus: DeviceBus)
+  def props[D <: Device](topicRoot: String,
+                         d: D,
+                         bus: DeviceBus,
+                         client: AsyncSimpleClient.Client)
                         (implicit s: Serialiser[D#Measurement]): Props = {
-    Props(new ResultsPublisher(topicRoot, d, bus))
+    Props(new ResultsPublisher(topicRoot, d, bus, client))
   }
 }
