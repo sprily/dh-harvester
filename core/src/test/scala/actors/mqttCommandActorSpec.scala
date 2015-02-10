3d89da058be522e94baf557f7d619149500c1aa5
Ian Murray <ian.c.murray@gmail.com>
2015-02-10 17:00:52 +0000

    (tests) Update old MqttCommandActor tests.

--- core/src/test/scala/actors/mqttCommandActorSpec.scala
+++ core/src/test/scala/actors/mqttCommandActorSpec.scala
@@ -2,6 +2,8 @@ package uk.co.sprily.dh
 package harvester
 package actors
 
+import scala.reflect.runtime.universe._
+
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.concurrent.Await
@@ -29,16 +31,16 @@ import uk.co.sprily.mqtt.MqttMessage
 
 import api.JsonUtils
 
-class MqttCommandActorSpec extends SpecificationLike
-                              with JsonUtils
-                              with NoTimeConversions {
+class ApiEndpointSpec extends SpecificationLike
+                         with JsonUtils
+                         with NoTimeConversions {
 
-  import MqttCommandActor.Types._
+  import ApiEndpoint.Types._
 
-  "A MqttCommandActor" should {
+  "A ApiEndpoint" should {
  
     "write a successful response back to the broker" in new Context {
-      val underTest = TestActorRef(new TestCommandActor())
+      val underTest = TestActorRef(new TestEndpoint())
       fakeModule.onDataReceived { (data: (Topic, List[Byte])) => testActor ! data }
       fakeModule.fakeIssueCommand("req-id-1", """{"name": "command-1"}""")
 
@@ -46,60 +48,62 @@ class MqttCommandActorSpec extends SpecificationLike
       val response = expectMsgType[(Topic, List[Byte])]
       val result = (new String(response._2.toArray, "UTF-8")).parseJson.asJsObject.convertTo[Res]
 
-      (response._1 must === (Topic("root/commands/req-id-1/response"))) &&
+      (response._1 must === (Topic("root/api/req-id-1/response"))) &&
       (result must === (expectedResult))
       checkRequestTopicCleared("req-id-1")
     }
 
     "respond to json de-serialisation errors by sending error response to broker" in new Context {
-      val underTest = TestActorRef(new TestCommandActor())
+      val underTest = TestActorRef(new TestEndpoint())
       fakeModule.onDataReceived { (data: (Topic, List[Byte])) => testActor ! data }
       fakeModule.fakeIssueCommand("req-id-2", """{}""")
 
       val response = expectMsgType[(Topic, List[Byte])]
       val error = new String(response._2.toArray, "UTF-8").parseJson.asJsObject
-      (response._1 must === (Topic("root/commands/req-id-2/response"))) &&
+      (response._1 must === (Topic("root/api/req-id-2/response"))) &&
       (error.fields.get("error") must not be (None))
       checkRequestTopicCleared("req-id-2")
     }
 
-    "respond to error result from child by sending response to broker" in new Context {
-      val underTest = TestActorRef(new TestCommandActor())
+    "respond to error result from worker by sending response to broker" in new Context {
+      val underTest = TestActorRef(new TestEndpoint())
       fakeModule.onDataReceived { (data: (Topic, List[Byte])) => testActor ! data }
       fakeModule.fakeIssueCommand("req-id-2", """{"name": "return-error"}""")
 
       val response = expectMsgType[(Topic, List[Byte])]
       val error = new String(response._2.toArray, "UTF-8").parseJson.asJsObject
-      (response._1 must === (Topic("root/commands/req-id-2/response"))) &&
+      (response._1 must === (Topic("root/api/req-id-2/response"))) &&
       (error.fields.get("error") must not be (None))
       checkRequestTopicCleared("req-id-2")
     }
 
-    "respond to exception in child by sending response to broker" in new Context {
-      val underTest = TestActorRef(new TestCommandActor())
+    "respond to exception in worker by sending response to broker" in new Context {
+      val underTest = TestActorRef(new TestEndpoint())
       fakeModule.onDataReceived { (data: (Topic, List[Byte])) => testActor ! data }
       fakeModule.fakeIssueCommand("req-id-2", """{"name": "throw-error"}""")
 
       val response = expectMsgType[(Topic, List[Byte])]
       val error = new String(response._2.toArray, "UTF-8").parseJson.asJsObject
-      (response._1 must === (Topic("root/commands/req-id-2/response"))) &&
+      (response._1 must === (Topic("root/api/req-id-2/response"))) &&
       (error.fields.get("error") must not be (None))
       checkRequestTopicCleared("req-id-2")
     }
 
   }
 
-  class Context extends AkkaSpecs2Support with ImplicitSender with DefaultJsonProtocol {
+  class Context extends AkkaSpecs2Support
+                   with ImplicitSender
+                   with DefaultJsonProtocol {
 
     final def checkRequestTopicCleared(id: String) = {
       val clear = expectMsgType[(Topic, List[Byte])]  // clearing the topic
-      (clear._1 must === (Topic(s"root/commands/$id/request"))) &&
+      (clear._1 must === (Topic(s"root/api/$id/request"))) &&
       (clear._2 must === (Nil))
     }
 
     import uk.co.sprily.mqtt._
 
-    def root = TopicPattern("root")
+    def root = Topic("root")
     lazy val fakeModule = new FakeMqttModule()
     lazy val fakeClient = Await.result(fakeModule.connect(MqttOptions.cleanSession()), 1.second)
 
@@ -108,22 +112,23 @@ class MqttCommandActorSpec extends SpecificationLike
     implicit def CmdJson: JsonFormat[Cmd] = jsonFormat1(Cmd)
     implicit def ResJson: JsonFormat[Res] = jsonFormat1(Res)
 
-    class TestCommandActor extends MqttCommandActor(root, fakeClient) {
+    class TestEndpoint extends ApiEndpoint(root, fakeClient) {
       type Command = Cmd
-      type Result = Res
-      override def commandJson = jsonFormat1(Cmd)
-      override def resultJson = jsonFormat1(Res)
-      override def childProps(id: RequestId, c: Command) = Props(new Actor() {
-
+      type Result  = Res
+      override def commandReader = jsonFormat1(Cmd)
+      override def resultWriter  = jsonFormat1(Res)
+      override val resultTypeTag = typeTag[Res]
+      override val timeout       = 5.seconds
+      
+      override val workerProps = Props(new Actor() {
         def receive = {
           case Cmd("return-error") =>
-            context.parent ! Response(id, TimedOutError.left)
+            context.parent ! response(TimedOutError.left)
           case Cmd("throw-error") =>
             throw new Exception("Something went wrong")
           case Cmd(other) =>
-            context.parent ! Response(id, Res(other.length).right)
+            context.parent ! response(Res(other.length).right)
         }
-
       })
     }
 
